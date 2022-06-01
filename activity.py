71ae5f22c3aaa338ed5f2b475e64da3e55e5731e
robert-huang <robert054321@hotmail.com>
2022-06-01 01:14:11 -0400

    refactor

--- activity.py
+++ activity.py
@@ -1,9 +1,14 @@
-from utils import URL, MAX_PAGE_SIZE, safe_post_request, depaginated_request
+from utils import safe_post_request, depaginated_request
 from oauth_utils import get_oauth_token
 import json
 import argparse
 from datetime import datetime
 import re
+
+REQUIRED_CONFIG_KEYS = [
+    "client_id",
+    "client_secret"
+]
 
 user_query = '''
 query ({0}) {{
@@ -35,12 +40,12 @@ query ({0}) {{
 '''
 
 query = '''
-query ($userId: Int!, $page: Int, $perPage: Int, $mediaType: ActivityType) {{
+query ($userId: Int!, $page: Int, $perPage: Int, $mediaTypes: [ActivityType]) {{
   Page (page: $page, perPage: $perPage) {{
     pageInfo {{
       hasNextPage
     }}
-    activities (userId: $userId, type: $mediaType, sort: ID) {{
+    activities (userId: $userId, type_in: $mediaTypes, sort: ID) {{
       ... on ListActivity {{
         media {{
           siteUrl
@@ -64,8 +69,8 @@ if __name__ == '__main__':
     parser.add_argument('-u', '--userId', default=839887)
     parser.add_argument('-n', '--username')
     parser.add_argument('-f', '--file', required=True)
-    parser.add_argument('-a', '--anime', action='store_true')
-    parser.add_argument('-m', '--manga', action='store_true')
+    parser.add_argument('-a', '--anime', action='append_const', dest='media_types', const='ANIME_LIST')
+    parser.add_argument('-m', '--manga', action='append_const', dest='media_types', const='MANGA_LIST')
     parser.add_argument('-d', '--full_date', action='store_true')
     parser.add_argument('-e', '--expand', action='store_true')
     parser.add_argument('-t', '--title_type', nargs='*',
@@ -73,16 +78,18 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--completed_only', action='store_true',
                         help='filters to completed entries only, ignored when the expand flag is used')
     parser.add_argument('-o', '--oauth_config',
-                        help='if a config file is provided with client_id and client_secret, run authenticated queries instead')
+                        help='if config file is provided, run authenticated queries instead')
     args = parser.parse_args()
-    if not (args.anime or args.manga):
+    if args.media_types is None:
         parser.error('one or more of the following arguments is required: -m/--manga, -a/--anime')
 
     oauth_token = None
     if args.oauth_config:
         with open(args.oauth_config) as f:
-            args.oauth_config = json.loads(f.read())
-        oauth_token = get_oauth_token(args.oauth_config['client_id'], args.oauth_config['client_secret'])
+            oauth_config = json.loads(f.read())
+        if missing_keys := [key for key in REQUIRED_CONFIG_KEYS if key not in oauth_config]:
+            raise Exception(f'Config is missing required keys: {missing_keys}')
+        oauth_token = get_oauth_token(oauth_config['client_id'], oauth_config['client_secret'])
 
     user_json = safe_post_request(
             {'query': user_query.format(
@@ -91,25 +98,26 @@ if __name__ == '__main__':
              'variables': {'userId': args.userId} if args.username is None else {'username': args.username}},
             oauth_token)
     user_id = user_json['User']['id']
-    mediaType = "MEDIA_LIST" if args.anime and args.manga else "MANGA_LIST" if args.manga else "ANIME_LIST"
-    activity = depaginated_request(query=query.format("\n".join(args.title_type)),
-                                   variables={'userId': user_id, 'mediaType': mediaType})
+    activity_list = depaginated_request(query=query.format("\n".join(args.title_type)),
+                                        variables={'userId': user_id, 'mediaTypes': args.media_types})
+
     with open(args.file, "w", encoding='utf8') as f:
-        f.write(json.dumps(user_json))`
+        f.write(json.dumps(user_json))
         f.write('\n')
-        activity_date_parsed = [(a | {"createdAt": datetime.fromtimestamp(a['createdAt']).strftime(
-            "%Y-%m-%d %H:%M:%S")}) if args.full_date else a for a in activity]
-        activity = []
+        activity_list_date_parsed = [(activity | {"createdAt": datetime.fromtimestamp(activity['createdAt']).strftime("%Y-%m-%d %H:%M:%S")})
+                                     if args.full_date else activity for activity in activity_list]
+        activity_list = []
         if args.expand:
-            for a in activity_date_parsed:
-                if a['status'] in {'watched episode', 'read chapter'} and (nums := re.search('([0-9]+) - ([0-9]+)', a['progress'])):
+            for activity in activity_list_date_parsed:
+                if (activity['status'] in {'watched episode', 'read chapter'}
+                        and (nums := re.search('([0-9]+) - ([0-9]+)', activity['progress']))):
                     start_num, end_num = nums.group(1, 2)
                     for num in range(int(start_num), int(end_num) + 1):
-                        activity.append(json.dumps(
-                            a | {"progress": str(num)}, ensure_ascii=False))
+                        activity_list.append(json.dumps(activity | {"progress": str(num)}, ensure_ascii=False))
                 else:
-                    activity.append(json.dumps(a, ensure_ascii=False))
+                    activity_list.append(json.dumps(activity, ensure_ascii=False))
         else:
-            activity = [json.dumps(a, ensure_ascii=False)
-                        for a in ([c for c in activity_date_parsed if c['status'] == 'completed'] if args.completed_only else activity_date_parsed)]
-        f.write("\n".join(activity))
+            activity_list = [json.dumps(activity, ensure_ascii=False) for activity in
+                             ([c for c in activity_list_date_parsed if c['status'] == 'completed']
+                              if args.completed_only else activity_list_date_parsed)]
+        f.write("\n".join(activity_list))
