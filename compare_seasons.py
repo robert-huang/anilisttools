383c843e2f64a0908b37340b070b67b346f295ae
robert-huang <robert054321@hotmail.com>
2026-04-11 19:32:56 -0400

    airing filter, average rating ignores unrated

--- compare_seasons.py
+++ compare_seasons.py
@@ -10,7 +10,7 @@ from request_utils import cache, safe_post_request, depaginated_request
 # TODO: Use MediaListCollection to get 500 entries at a time instead of 50
 # TODO: Proper object-oriented library with e.g. User.shows(fields=[...])
 @cache(".cache/user_list_seasons.json", max_age=timedelta(minutes=15))
-def get_user_shows(username, status_in=('COMPLETED',)) -> list:
+def get_user_shows(username, status_in=('COMPLETED',), airing: bool = False) -> list:
     """Given an AniList user ID, fetch the user's anime with given statuses, returning a list of show
      JSONs, including and sorted on score (desc).
      Include season and seasonYear.
@@ -35,14 +35,17 @@ query ($userName: String, $statusIn: [MediaListStatus], $page: Int, $perPage: In
                 seasonYear
             }
             score
+            notes
         }
     }
 }'''
 
     return [{**list_entry['media'], 'score': list_entry['score']}  # Stuff score in too
-            for list_entry in depaginated_request(query=query, variables={'userName': username, 'statusIn': status_in})]
+            for list_entry in depaginated_request(query=query, variables={'userName': username, 'statusIn': status_in})
+            if not airing or (list_entry['notes'] and "#airing" in list_entry['notes'])]# and list_entry['score'] > 0)]
 
 # python.exe compare_seasons.py robert -l 40 "winter 2020" "spring 2020" "summer 2020" "fall 2020" "winter 2021" "spring 2021" "summer 2021" "fall 2021" "winter 2022" "spring 2022" "summer 2022" "fall 2022" "winter 2023" "spring 2023" "summer 2023" "fall 2023" "winter 2024" "spring 2024" "summer 2024" "fall 2024" "winter 2025" "spring 2025" "summer 2025" "fall 2025" "winter 2026" "spring 2026" "summer 2026" "fall 2026" > seasons.txt
+# python.exe compare_seasons.py robert -asl 40 "allseasons" > seasons.txt
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Given an AniList username, check what shows from their completed or planning lists have known\n"
@@ -54,9 +57,10 @@ if __name__ == '__main__':
                              'Seasons are Winter, Spring, Summer, Fall.')
     parser.add_argument("-l", "--show-length", help="Override show length displayed")
     parser.add_argument("-s", "--skip-empty", action="store_true", help="Skip empty years/seasons")
+    parser.add_argument("-a", "--airing", action="store_true", help="Filter to entries with #airing in its notes")
     args = parser.parse_args()
 
-    user_shows = get_user_shows(args.username, status_in=('COMPLETED', 'CURRENT', 'REPEATING'))
+    user_shows = get_user_shows(args.username, status_in=('COMPLETED', 'CURRENT', 'REPEATING'), airing=args.airing)
 
     if args.seasons == ['all']:
         args.seasons = [str(year) for year in range(min([show['seasonYear'] if show['seasonYear'] else 9999 for show in user_shows]),
@@ -91,8 +95,8 @@ if __name__ == '__main__':
             return True
 
     print('   '.join([pad(season, DISPLAY_LENGTH) for i, season in enumerate(args.seasons) if display_season(seasonal_user_shows[i])]))
-    print('   '.join([pad('     avg: ' + str(round(sum([show['score'] for show in shows]) / len(shows), 3) if len(shows) > 0 else "N/A"), DISPLAY_LENGTH)
-                      for shows in seasonal_user_shows if display_season(shows)]))
+    print('   '.join([pad('     avg: ' + str(round(sum([show['score'] for show in rated_shows]) / len(rated_shows), 3) if len(rated_shows) > 0 else "N/A"), DISPLAY_LENGTH)
+                      for rated_shows in [[show for show in shows_per_season if show['score'] > 0] for shows_per_season in seasonal_user_shows] if display_season(rated_shows)]))
     print("=" * ((DISPLAY_LENGTH + 3) * len([shows for shows in seasonal_user_shows if display_season(shows)]) - 3))
     for i in range(max(len(shows) for shows in seasonal_user_shows)):
         print('   '.join(pad(shows[i]['score'], 3) + '  ' + pad(shows[i]['title']['english']
